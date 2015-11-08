a279633c957ee8dfc735258ba08e3e045eeef768
diegodoumecq <deelede@yahoo.com.ar>
2015-11-08 03:37:46 -0300

    Solution

--- controller.js
+++ controller.js
@@ -1,4 +1,19 @@
-angular.module('test', [])
-.controller('testController', ['$scope', function($scope) {
-	
-}]);
+angular.module('test', []);
+
+angular.module('test')
+    .controller('testController', ['$scope', function (s) {
+        s.parity = 'both';
+        s.position = 'left';
+        s.boundaries = {
+            min: 0,
+            max: 0
+        };
+
+        s.setPosition = function (value) {
+            s.position = value;
+        };
+
+        s.setParity = function (value) {
+            s.parity = value;
+        };
+    }]);
