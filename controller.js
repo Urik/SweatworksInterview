aa6dab4db391c224cd6ce09f587d15d2bea880be
gabriel <gamatusevich@gmail.com>
2015-11-12 14:39:26 -0300

    Adding some comments

--- controller.js
+++ controller.js
@@ -1,7 +1,18 @@
 'use strict';
 
+/**
+ * Testapp for Sweatworks repo
+ * I'm using an All-Directives approach base on this article https://medium.com/@bluepnume/sane-scalable-angular-apps-are-tricky-but-not-impossible-lessons-learned-from-paypal-checkout-c5320558d4ef
+ * because this Component-based code style is very similar to Angular 2 & React and it helps organize the project
+ */
+
+// No need to use the array form ['$scope', ...] since we are not minimizing
+// Tools like gulp/grunt have plugins such us ngAnnotate which add the angular annotations when minimizing
 angular.module('test', [])
-  // No need to use the array form ['$scope', ...] since we are not minimizing
+  /**
+   * Main Component
+   * Contains the entire application
+   */
   .directive('testApp', function () {
     return {
       restrict    : 'E',
@@ -19,6 +30,7 @@ angular.module('test', [])
         };
 
         appCtrl.alignment = 'left';
+        appCtrl.parity = null;
 
         appCtrl.changeAlignment = function changeAligment (alignment) {
           appCtrl.alignment = alignment;
@@ -28,17 +40,14 @@ angular.module('test', [])
           appCtrl.parity = parity;
         }
 
-        appCtrl.parity = null;
-
       },
-      controllerAs  : 'appCtrl',
-      link : {
-        post : function postLink (scope, element, attributes) {
-
-        }
-      }
+      controllerAs  : 'appCtrl'
+      // No need for linking function since we are not manipulating the DOM, just adding some logic
     }
   })
+  /**
+   * Panel Component, switchs content left or right based on the 'alignment' parameter
+   */
   .directive('testPanel' ,function () {
     return {
       restrict    : 'E',
@@ -57,6 +66,11 @@ angular.module('test', [])
       }
     }
   })
+  /**
+   * List Component
+   * Renders a list of numbers based on the min/max values and displays accordingly to the parity argument
+   * null -> all numbers, true -> even numbers, false -> odd numbers
+   */
   .directive('testList', function () {
     return {
       restrict    : 'E',
@@ -97,6 +111,10 @@ angular.module('test', [])
       }
     }
   })
+  /**
+   * Parity Filter
+   * Checks a collection of numbers for parity and returns a new collection based on the 'parity' argument
+   */
   .filter('parityFilter', function () {
     return function (items, parity) {
       var filtered = [];
@@ -107,12 +125,18 @@ angular.module('test', [])
       }
 
       filtered = items.filter(function (item) {
-        var even = (item % 2 === 0);
+
+        if (typeof item !== 'number') {
+          throw new TypeError('Expecting a collection of numbers');
+        }
 
         if (parity === null) {
           return true;
         }
 
+        var even = (item % 2 === 0);
+
+        // XOR does the trick!
         return even ^ !parity;
 
       });
@@ -121,6 +145,9 @@ angular.module('test', [])
 
       }
   })
+  /**
+   * Setting up some references values so we don't make Typos ;)
+   */
   .factory('Alignments', function () {
     return {
       LEFT  : 'left',
