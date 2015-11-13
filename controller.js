38dc8407d47d06d9dca6c0f97738a15829836fd4
Tomas Bocanera <tomasboc@gmail.com>
2015-11-13 18:59:09 -0300

    Bocanera Tomas SweatworksInterview Test

--- controller.js
+++ controller.js
@@ -1,4 +1,50 @@
 angular.module('test', [])
 .controller('testController', ['$scope', function($scope) {
-	
-}]);
+	$scope.values = {
+		minValue: 0,
+		maxValue: 1
+	};
+
+	$scope.numberTypes = {
+		evenNumbers: false,
+		oddNumbers: false
+	};
+
+	$scope.updateList = function (){
+		$scope.numbers = [];
+		for(var i=$scope.values.minValue; i <= $scope.values.maxValue; i++){
+			$scope.numbers.push(i);
+		}
+	};
+
+	$scope.displayOnTheLeft = function () {
+		$scope.displayOnClass = "on-left-side";
+	};
+
+	$scope.displayOnTheRight = function () {
+		$scope.displayOnClass = "on-right-side";
+	};
+
+	$scope.showAll = function() {
+		$scope.numberTypes.evenNumbers = false;
+		$scope.numberTypes.oddNumbers = false;
+	};
+
+	$scope.showEvens = function() {
+		$scope.numberTypes.evenNumbers = true;
+		$scope.numberTypes.oddNumbers = false;
+	};
+
+	$scope.showOdds = function() {
+		$scope.numberTypes.evenNumbers = false;
+		$scope.numberTypes.oddNumbers = true;
+	};	
+
+	$scope.updateList();
+}])
+.directive('numbers', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'numbers.html'
+	};
+});
