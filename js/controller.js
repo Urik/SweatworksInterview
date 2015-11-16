445094d52f08fa39b419f38dca2f36d8d8d0dad2
Sergio <sergeen@gmail.com>
2015-11-16 11:37:23 -0300

    deleted filters.js added some comments

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 angular.module('controller', [])
     .controller('testController', ['$scope', function($scope) {
-
+        //creates an array of odd numbers between the 2 input numbers
         $scope.oddArray = function() {
             $scope.listValues = [];
             for (var i = $scope.minValue; i <= $scope.maxValue; i++) {
@@ -9,7 +9,7 @@ angular.module('controller', [])
                 }
             }
         };
-
+        //creates an array of Even numbers between the 2 input numbers
         $scope.evenArray = function() {
             $scope.listValues = [];
             for (var i = $scope.minValue; i <= $scope.maxValue; i++) {
@@ -18,7 +18,7 @@ angular.module('controller', [])
                 }
             }
         };
-
+        //creates an array of  numbers between the 2 input numbers
         $scope.listArray = function() {
             $scope.listValues = [];
             for (var i = $scope.minValue; i <= $scope.maxValue; i++) {
--- js/filters.js
+++
@@ -1,8 +0,0 @@
-angular.module('filters', [])
-    .directive('testFilters', function() {
-
-        return {
-            restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
-            template: '<div ng-repeat = "value in listValues track by $index">{{value}}</div><br>',
-        };
-    });
